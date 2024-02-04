4f3473c62aebb3ec5d8d8836a3e1384be856e8af
LyLy Tran <lylytr.311@gmail.com>
2024-02-03 19:02:47 -0500

    creating wallpaper

---
+++ js/newtab.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var imgUrl = 'https://source.unsplash.com/random/1920x1080/?pastel';
+    document.getElementById('wallpaperImage').src = imgUrl;
+});
+
+
